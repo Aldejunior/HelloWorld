5f7045983018f0a42032f58172379631dd47ce23
Aldejunior <aldejunioroliveira@gmail.com>
2023-04-05 22:34:41 -0300

    vou dormir

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Coisa Linda!</Text>
+      <Text>Sono muito, sono.</Text>
     </View>
   );
 }
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#CCC',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start'
   },
+  titulo:{
+    backgroundColor: '#FF69B4',
+    padding: 10,
+    borderRadius: 5
+  }
 });
